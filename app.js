fe8b99a370b7e840b18ca2e33c52950fcb0847d4
nivabecassis <abecassis.niv@gmail.com>
2022-05-15 15:34:37 +0300

    Adding app.js and config

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Load env configs
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to the db
+connectDB();
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+if (process.env.NODE_ENV === "dev") {
+  app.use(logger("combined"));
+}
+
+// Routes
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// Set express' static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// TODO add middleware for errors
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta
+      .bold
+  )
+);
+
+// Handle unhandled promises and crash server
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close((err) => process.exit(1));
+});
 
 module.exports = app;
