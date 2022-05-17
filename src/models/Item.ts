4a584946320c7867b128d0dfdcb6bc3ddd6c8213
nivabecassis <abecassis.niv@gmail.com>
2022-05-17 18:59:30 +0300

    Remove config.env from git

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -12,6 +12,14 @@ const ItemSchema: Schema = new Schema({
   color: { type: String, required: true },
 });
 
+// TODO after updating the sku, change values in locations
+// TODO after updating the color, verify if sku was changed (otherwise change the sku auto)
+// TODO before transferring stock, verify if there is enough in origin location
+
+ItemSchema.pre(["updateOne", "findOneAndUpdate"], async (next) => {
+  if ()
+})
+
 const Item: Model<IItem> = model("Item", ItemSchema);
 
 export default Item;
